510d1a56670758ecdd050c0876640c3c1888881c
pea-nut-z <topeanutz@gmail.com>
2021-06-20 22:26:57 -0400

    render images logic

--- src/App.js
+++ src/App.js
@@ -1,54 +1,106 @@
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+// 2021-6-16
 
 function App() {
-  // const getTodayDateString = () => {
-  //   const date = new Date();
-  //   const year = date.getUTCFullYear();
-  //   const month = date.getUTCMonth() + 1;
-  //   const day = date.getUTCDate() - 1;
-  //   return `${year}-${month}-${day}`;
-  // };
+  const [frontViewImg, setFrontViewImg] = useState();
+  const [otherImgs, setotherImgs] = useState();
 
-  // const currentDate = getTodayDateString();
-  // console.log({ currentDate });
-  const monthsWith30Days = [];
-  const date = new Date();
-  let year = date.getUTCFullYear();
-  let month = date.getUTCMonth() + 1;
-  let day = date.getUTCDate() - 1;
+  const getTodayDate = () => {
+    const date = new Date();
+    const year = date.getUTCFullYear();
+    const month = date.getUTCMonth() + 1;
+    const day = date.getUTCDate() - 1;
+    return [year, month, day];
+  };
 
-  console.log(typeof year);
+  const getPreviousMonthDate = (year, month) => {
+    const monthsWith30Days = [4, 6, 9, 11];
+    const preMonth = month === 1 ? 12 : month - 1;
+    let preYear = year;
+    let preDay;
 
-  // const getPhotos = (year, month, day) => {
+    if (preMonth === 2) {
+      preDay = 28;
+    } else if (monthsWith30Days.includes(preMonth)) {
+      preDay = 30;
+    } else {
+      preYear = preMonth === 12 && year - 1;
+      preDay = 31;
+    }
+    return [preYear, preMonth, preDay];
+  };
 
-  //   const API_URL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2021-6-16&page=1&api_key=${process.env.REACT_APP_API_KEY}`;
-  //   fetch(API_URL)
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       if (data.photos.length === 0) {
-  //         if(day === 1 && )
-  //         day ===
-  //         day = day - 1
+  const getOtherImgs = async (year, month, day) => {
+    const cameras = ["RHAZ", "MAST", "CHEMCAM", "MAHLI", "MARDI", "NAVCAM"];
+    const images = [];
+    await cameras.forEach((camera) => {
+      const API_URL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?camera=${camera}&earth_date=${year}-${month}-${day}&page=1&api_key=${process.env.REACT_APP_API_KEY}`;
+      fetch(API_URL)
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.photos.length === 0) {
+            return;
+          }
+          const image = data.photos[0];
+          images.push(image);
+          console.log({ images });
+        });
+    });
+    setotherImgs(images);
+  };
 
-  //       }
-  //       console.log();
-  //     });
-  // };
+  const getPhotos = (array) => {
+    let [year, month, day] = array;
+    const API_URL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?camera=FHAZ&earth_date=${year}-${month}-${day}&page=1&api_key=${process.env.REACT_APP_API_KEY}`;
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then(async (data) => {
+        if (data.photos.length === 0) {
+          if (day === 1) {
+            const previousMonthDate = getPreviousMonthDate(year, month);
+            getPhotos(previousMonthDate);
+          } else {
+            getPhotos([year, month, day - 1]);
+          }
+        } else {
+          const image = data.photos[0];
+          await getOtherImgs(year, month, day);
 
-  // getPhotos(year, month, day);
+          setFrontViewImg(image);
+        }
+      })
+      .catch((error) => {
+        console.log("There is an error.", error);
+      });
+    // .finally(() => {
+    //   console.log(images);
+
+    //   setFrontViewImg(images);
+    // });
+  };
+
+  useEffect(() => {
+    const todayDate = getTodayDate();
+    getPhotos(todayDate);
+  }, []);
+
+  // useEffect(() => {
+  //   console.log(frontViewImg);
+  // });
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      {otherImgs &&
+        otherImgs.map((image, index) => {
+          console.log(image);
+          return (
+            <img key={index} src={image.img_src} alt="Image of Mars taken by Curiosity Rover" />
+          );
+        })}
+      {frontViewImg && (
+        <img src={frontViewImg.img_src} alt="Image of Mars taken by Curiosity Rover" />
+      )}
     </div>
   );
 }
